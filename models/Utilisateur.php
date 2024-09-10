7c588de659f3487b7c8024c707e02bec64d55219
Baldium <alexandre.marescaux@outlook.fr>
2024-09-10 09:09:40 +0200

    1er push et mise en place de la methode mvc ainsi que du model Utilisateur

---
+++ models/Utilisateur.php
@@ -0,0 +1,35 @@
+<?php
+
+class Utilisateur
+{
+    public function __construct()
+    {
+        $this->table = "utilisateurs";
+        $this->getBdd();
+    }
+
+    public function getUtilisateur($id)
+    {
+        return $this->requete("SELECT * FROM utilisateurs WHERE id = ?", [$id]);
+    }
+
+    public function getUtilisateurs()
+    {
+        return $this->requete("SELECT * FROM utilisateurs");
+    }
+
+    public function addUtilisateur($nom, $prenom, $email, $password)
+    {
+        return $this->requete("INSERT INTO utilisateurs (nom, prenom, email, password) VALUES (?, ?, ?, ?)", [$nom, $prenom, $email, $password]);
+    }
+
+    public function updateUtilisateur($id, $nom, $prenom, $email, $password)
+    {
+        return $this->requete("UPDATE utilisateurs SET nom = ?, prenom = ?, email = ?, password = ? WHERE id = ?", [$nom, $prenom, $email, $password, $id]);
+    }
+
+    public function deleteUtilisateur($id)
+    {
+        return $this->requete("DELETE FROM utilisateurs WHERE id = ?", [$id]);
+    }
+}
