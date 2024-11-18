de03dc7d62634c5c13bb265d894295b39043ba80
N-ASSAF <a.nufil@lprs.com>
2024-11-18 15:43:17 +0100

    Correction de certains beugs entre aide avec ayoub

--- view/view_etudiants/accueil.php
+++ view/view_etudiants/accueil.php
@@ -79,7 +79,7 @@ $posts = $pp_req->fetchAll(PDO::FETCH_ASSOC);
       <div class="menu-item" onclick="window.location.href='./mes_favoris.php';" style="cursor: pointer;">Mes Offres Favorites</div>
       <div class="menu-item" onclick="window.location.href='./actualites.php';" style="cursor: pointer;">Actualités (Ayoub)</div>
       <div class="menu-item" onclick="window.location.href='../viewEvent/creer_evenement.php';" style="cursor: pointer;">Événements (Ayoub)</div>
-      <div class="menu-item" onclick="window.location.href='./post.php';" style="cursor: pointer;">Post</div>
+      <div class="menu-item" onclick="window.location.href='../view_post/gestion.html';" style="cursor: pointer;">Post</div>
       <div class="menu-item" onclick="window.location.href='../view_business/connexion_business.php';" style="cursor: pointer;">Pour Les Entreprises</div>
       <div class="menu-item" onclick="window.location.href='./entreprises_partenaire.php';" style="cursor: pointer;">Entreprises Partenaires ()</div>
       <div class="menu-item" onclick="window.location.href='./qui-sommes-nous.html';" style="cursor: pointer;">Qui sommes-nous ?</div>
@@ -216,7 +216,7 @@ $posts = $pp_req->fetchAll(PDO::FETCH_ASSOC);
         const isLiked = button.hasClass('liked'); // Vérifier si le bouton est déjà liké
 
         $.ajax({
-            url: '../../controller/controllerAlumnis/like_post.php',
+            url: '../../controller/controllerAlumis/like_post.php',
             method: 'POST',
             data: {
                 postId: postId,
